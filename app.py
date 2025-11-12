beb4e03ed1e533bf4964caf5276ec42b2024cf20
mohamadjiha <44312125+mohamadjiha@users.noreply.github.com>
2025-11-12 06:05:41 +0200

    app.py

--- app.py
+++ app.py
@@ -18,4 +18,7 @@ def check_license():
         return jsonify({"status": "error", "msg": "Invalid license ❌"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
+
